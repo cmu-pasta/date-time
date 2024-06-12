c132848a77d5ff480ee7dee6c2901e6cacd53945
Ajoukov <aapteryx@gmail.com>
2024-06-12 15:30:45 -0400

    created a global paths file

---
+++ scripts/global_paths.py
@@ -0,0 +1,9 @@
+DATA_DIR = "../data/"
+
+REPOS_PATH = DATA_DIR + "repos.csv"
+
+CLONE_REPOS_DIR = DATA_DIR + "cloned_repos/"
+REPOS_WITH_DATETIME_PATH = DATA_DIR + "repos_with_datetime.csv"
+
+ISSUES_PATH = DATA_DIR + "issues.csv"
+DIFFS_PATH = DATA_DIR + "diffs.csv"
