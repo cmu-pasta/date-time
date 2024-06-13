8ee3c6622afc7da5550a87a30b6a00c7e9d08235
Ajoukov <aapteryx@gmail.com>
2024-06-13 12:55:10 -0400

    finished collecting repos

--- scripts/global_paths.py
+++ scripts/global_paths.py
@@ -1,8 +1,8 @@
-DATA_DIR = "../data/"
+DATA_DIR = "/data/sjoukov/date-time/data/"
 
 REPOS_PATH = DATA_DIR + "repos.csv"
 
-CLONE_REPOS_DIR = DATA_DIR + "cloned_repos/"
+CLONE_REPOS_DIR = DATA_DIR + "cloned_repos2/"
 REPOS_WITH_DATETIME_PATH = DATA_DIR + "repos_with_datetime.csv"
 
 ISSUES_PATH = DATA_DIR + "issues.csv"
