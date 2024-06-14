0aff0e045bdc58df238e7224e0d5d914c7d195d6
Ajoukov <aapteryx@gmail.com>
2024-06-14 11:20:38 -0400

    new filtering scripts

--- scripts/global_paths.py
+++ scripts/global_paths.py
@@ -2,8 +2,10 @@ DATA_DIR = "/data/sjoukov/date-time/data/"
 
 REPOS_PATH = DATA_DIR + "repos.csv"
 
-CLONE_REPOS_DIR = DATA_DIR + "cloned_repos2/"
+CLONE_REPOS_DIR = DATA_DIR + "cloned_repos4/"
 REPOS_WITH_DATETIME_PATH = DATA_DIR + "repos_with_datetime.csv"
+FILTERED_REPOS_PATH = DATA_DIR + "repos_with_grep_and_loc.csv"
+LOG_DIR = DATA_DIR + "logs/"
 
 ISSUES_PATH = DATA_DIR + "issues.csv"
 DIFFS_PATH = DATA_DIR + "diffs.csv"
