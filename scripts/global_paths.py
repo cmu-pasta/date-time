65cde01b7454e672220c254220306b728b4a96e2
Ajoukov <aapteryx@gmail.com>
2024-06-17 17:57:57 -0400

    cleaned up scripts repo

--- scripts/global_paths.py
+++ scripts/global_paths.py
@@ -1,5 +1,8 @@
 DATA_DIR = "/data/sjoukov/date-time/data/"
 
+GH_ACCESS_TOKEN = "access_tokens/gh_access_token"
+SG_ACCESS_TOKEN = "access_tokens/sg_access_token"
+
 REPOS_PATH = DATA_DIR + "repos.csv"
 
 CLONE_REPOS_DIR = DATA_DIR + "cloned_repos4/"
