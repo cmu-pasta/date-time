88ffdbdda32299235ececf94fea5b101fd8e2a06
Ajoukov <aapteryx@gmail.com>
2024-06-26 15:02:38 -0400

    added repo counting script

---
+++ scripts/2_get_repo_statistics.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from __global_paths import *
+
+df = pd.read_csv(SEPARATED_FILTERED_REPOS_PATH)
+
+cols = ['grep_results0', 'grep_results1', 'grep_results2', 'grep_results3', 'grep_results4']
+counts = df[cols].sum()
+
+print(f"""
+Datetime:     {counts[cols[0]]}
+Arrow:        {counts[cols[1]]}
+Pendulum:     {counts[cols[2]]}
+Whenever:     {counts[cols[3]]}
+Heliclockter: {counts[cols[4]]}
+"""
+)
