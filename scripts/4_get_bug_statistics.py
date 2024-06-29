5a6fda2f05e1712e53b07669c9bae8809c28cbfc
Ajoukov <aapteryx@gmail.com>
2024-06-29 14:30:16 -0400

    new words list

--- scripts/4_get_bug_statistics.py
+++ scripts/4_get_bug_statistics.py
@@ -41,12 +41,12 @@ print({
 })
 
 words="""
-datetime    pytz    leap    strptime    interval    microsecond
-timestamp   dateutil    DST strftime    duration    nanosecond
-    arrow   daylight    utcnow  elapsed millisecond
-        whenever    year    fromtimestamp   timedelta   second
-        tzinfo  pendulum    localtime   GMT fold    timezone
-        calendar    heliclockter    epoch   UTC     date time
+datetime    pytz    leap    strptime    microsecond
+timestamp   dateutil    DST strftime    nanosecond
+tzinfo  arrow   daylight    utcnow  millisecond
+epoch   pendulum    year    fromtimestamp   timezone
+timedelta   UTC localtime   GMT interval
+fold    elapsed duration        
 """.split()
 
 print(f"NUM_WORDS: {len(words)}")
