960bbc1ca2702cec3f160653074fb6262db47c94
Shrey <smtiwari@andrew.cmu.edu>
2024-06-30 19:42:40 -0400

    updating

---
+++ benchmarks/test_deprecated.py
@@ -0,0 +1,32 @@
+"""
+Description: This file contains code snippets that make use of deprecated date-time library API methods. 
+
+Links:
+- https://blog.ganssle.io/articles/2019/11/utcnow.html
+- https://discuss.python.org/t/deprecating-utcnow-and-utcfromtimestamp/26221
+- https://blog.miguelgrinberg.com/post/it-s-time-for-a-change-datetime-utcnow-is-now-deprecated#:~:text=The%20problem%20that%20the%20Python,is%20already%20known%20in%20advance.
+"""
+
+import unittest
+from datetime import datetime, timezone
+
+from hypothesis import given
+from hypothesis.strategies import integers, timezones
+
+
+class TestDeprecatedAPIUsage(unittest.TestCase):
+    def test_deprecated_api_usage_0(self):
+        dt_utcnow = datetime.utcnow()
+        dt_now = datetime.now(tz=timezone.utc)
+        self.assertEqual(dt_utcnow, dt_now)
+
+    @given(integers(min_value=0, max_value=4294967296), timezones())
+    def test_deprecated_api_usage_1(self, timestamp: int, timezone: timezone):
+        dt = datetime.utcfromtimestamp(timestamp)
+        ts_new = dt.astimezone(tz=timezone).timestamp()
+        self.assertEqual(ts_new, timestamp)
+
+
+# python -W ignore::DeprecationWarning your_test_script.py
+if __name__ == "__main__":
+    unittest.main()
