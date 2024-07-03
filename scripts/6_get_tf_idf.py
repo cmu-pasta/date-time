bbf57fbeb75fa4f146e5962856627432add69bfb
Peter Vandervelde <petervan@andrew.cmu.edu>
2024-07-03 16:41:56 -0400

    tokenize with nltk

--- scripts/6_get_tf_idf.py
+++ scripts/6_get_tf_idf.py
@@ -3,15 +3,34 @@ import pandas as pd
 import numpy as np
 import math
 import re
+from nltk.tokenize import word_tokenize
+
+try:
+    word_tokenize("test")
+except:
+    import nltk
+    nltk.download('punkt')
+
+def tokenize(comment):
+    tokens = word_tokenize(comment)
+    tokens = [t for t in tokens if re.search("[a-zA-Z]", t) is not None]
+    return tokens
+
+def count_matches(tokens):
+    ans = np.zeros(len(KEYWORDS_RAW))
+    for i,keyword in enumerate(KEYWORDS_RAW):
+        for word in tokens:
+            if word.lower().startswith(keyword.lower()):
+                ans[i] += 1
+    return ans
 
 def compute_tf(comment):
-    words = re.split(r'[^a-zA-Z]+', comment)
+    words = tokenize(comment)
     # words = comment.split()
     word_count = len(words)
     if word_count == 0:
         return np.zeros(len(KEYWORDS_RAW))
-    tf = np.array([(len([word for word in words if word.lower().startswith(keyword.lower())])) / word_count for keyword in KEYWORDS_RAW])
-    return tf
+    return count_matches(words) / word_count
 
 # calculate IDF
 repr_df = pd.read_csv(ISSUE_REPR_SAMPLE_PATH)
@@ -21,8 +40,8 @@ for i, row in repr_df.iterrows():
     index = row["id"]
     with open(f"{COMMENTS_DIR}{index}", "r") as file:
         comment = file.read()
-        words = re.split(r'[^a-zA-Z]+', comment)
-        appearances = [(len([word for word in words if word.lower().startswith(keyword.lower())])) for keyword in KEYWORDS_RAW]
+        words = tokenize(comment)
+        appearances = count_matches(words)
         for j in range(len(KEYWORDS_RAW)):
             if appearances[j] != 0:
                 keyword_counts[j]+=1
