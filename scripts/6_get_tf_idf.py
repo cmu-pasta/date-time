74439c725ca96db47913ab361e3faeb9d65eaf71
Peter Vandervelde <petervan@andrew.cmu.edu>
2024-07-08 09:55:44 -0400

    added the tf_idf scores for individual keywords

--- scripts/6_get_tf_idf.py
+++ scripts/6_get_tf_idf.py
@@ -5,6 +5,8 @@ import math
 import re
 from nltk.tokenize import word_tokenize
 
+# By default, nltk doesn't come with all the data it needs to run, so we need to run this download command
+# to grab the rest of the data
 try:
     word_tokenize("test")
 except:
@@ -13,9 +15,13 @@ except:
 
 def tokenize(comment):
     tokens = word_tokenize(comment)
+    # only include tokens that contain a letter
+    # this will also include things like the "'s" in it's -> ["it", "'s"]
     tokens = [t for t in tokens if re.search("[a-zA-Z]", t) is not None]
     return tokens
 
+# a word matches if it contains the keyword as a prefix. 
+# Since this will catch plurals, we only use singular keywords
 def count_matches(tokens):
     ans = np.zeros(len(KEYWORDS_SINGULAR))
     for i,keyword in enumerate(KEYWORDS_SINGULAR):
@@ -26,7 +32,6 @@ def count_matches(tokens):
 
 def compute_tf(comment):
     words = tokenize(comment)
-    # words = comment.split()
     word_count = len(words)
     if word_count == 0:
         return np.zeros(len(KEYWORDS_SINGULAR))
@@ -46,6 +51,7 @@ for i, row in repr_df.iterrows():
             if appearances[j] != 0:
                 keyword_counts[j]+=1
     
+print("Finished calculating TF-IDF")
 
 idf = np.log(repr_df.shape[0] / keyword_counts)
 pd.DataFrame(idf, columns=["idf"]).to_csv(IDFS_PATH, index=False)
@@ -56,9 +62,10 @@ for i, row in bugs_df.iterrows():
     index = row["id"]
     with open(f"{COMMENTS_DIR}{index}", "r") as file:
         comments = file.read()
-        # bugs_df.at[i, "comments"] = comments
         tf = compute_tf(comments)
+        indivivdual_tf_idf = tf * idf
+        for j, word in enumerate(KEYWORDS_SINGULAR):
+            bugs_df.at[i, "td_idf_"+word] = indivivdual_tf_idf[j]
         tf_idf = tf.dot(idf)
         bugs_df.at[i, "tf_idf"] = tf_idf
-
 bugs_df.to_csv(BUGS_WITH_TF_IDF_PATH, index=False)
