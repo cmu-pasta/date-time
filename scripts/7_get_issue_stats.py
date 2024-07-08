eb9206e0facaaf4ff343586de5aa4a8b43649dc2
Alexander Joukov <92131019+Ajoukov@users.noreply.github.com>
2024-07-08 13:39:02 -0400

    Plurals word list (#33)
    * added nltk to deps
    
    * taking the top half of tf_idf
    
    * added all files

--- scripts/7_get_issue_stats.py
+++ scripts/7_get_issue_stats.py
@@ -6,11 +6,14 @@ K = 100
 bugs_with_stats_df = pd.merge(pd.read_csv(BUGS_WITH_TF_IDF_PATH), pd.read_csv(REPOS_PATH), left_on='repoName', right_on='nameWithOwner', how='left')
 bugs_with_stats_df.to_csv(BUGS_WITH_STATS_PATH, index=False)
 
+bugs_with_good_tf_idf_df = bugs_with_stats_df.sort_values(by="tf_idf", ascending=False).head(len(bugs_with_stats_df)//2)
+
 top_k_dfs = []
 
-categories = ["stars", "tf_idf", "timelineCount", "issuesCount", "forkCount", "watchersCount", "discussionsCount"]
+#categories = ["stars", "tf_idf", "timelineCount", "issuesCount", "forkCount", "watchersCount", "discussionsCount"]
+categories = ["tf_idf", "stars", "timelineCount", "issuesCount", "forkCount", "watchersCount", "discussionsCount"]
 for category in categories:
-    sorted_category_df = bugs_with_stats_df.sort_values(by=category, ascending=False)
+    sorted_category_df = bugs_with_good_tf_idf_df.sort_values(by=category, ascending=False)
     top_k_dfs.append(sorted_category_df.head(K))
 
 bugs_from_top_in_categories = pd.concat(top_k_dfs).drop_duplicates()
