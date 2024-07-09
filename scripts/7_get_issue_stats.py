94c7107ada701919252fd9262e3b3dc0d0070133
Peter Vandervelde <petervan@andrew.cmu.edu>
2024-07-09 16:57:37 -0400

    filter out issues with less than 500 chars

--- scripts/7_get_issue_stats.py
+++ scripts/7_get_issue_stats.py
@@ -2,6 +2,7 @@ from __global_paths import *
 import pandas as pd
 
 K = 200
+SIZE_MIN = 500
 
 bugs_with_stats_df = pd.merge(pd.read_csv(BUGS_WITH_TF_IDF_PATH), pd.read_csv(REPOS_PATH), left_on='repoName', right_on='nameWithOwner', how='left')
 bugs_with_stats_df.to_csv(BUGS_WITH_STATS_PATH, index=False)
@@ -15,14 +16,14 @@ new_column_order = [
 
 bugs_with_stats_df = bugs_with_stats_df[new_column_order]
 
-bugs_with_good_tf_idf_df = bugs_with_stats_df.sort_values(by="tf_idf", ascending=False).head(int(len(bugs_with_stats_df) * 0.2))
-bugs_with_good_size_df = bugs_with_good_tf_idf_df.sort_values(by="size", ascending=False).head(int(len(bugs_with_good_tf_idf_df) * 0.5))
+bugs_with_good_size_df = bugs_with_stats_df[bugs_with_stats_df["size"] >= SIZE_MIN]
+bugs_with_good_tf_idf_df = bugs_with_good_size_df.sort_values(by="tf_idf", ascending=False).head(int(len(bugs_with_good_size_df) * 0.2))
 
 top_k_dfs = []
 
 categories = ["tf_idf", "stars", "size"]
 for category in categories:
-    sorted_category_df = bugs_with_good_size_df.sort_values(by=category, ascending=False)
+    sorted_category_df = bugs_with_good_tf_idf_df.sort_values(by=category, ascending=False)
     top_k_dfs.append(sorted_category_df.head(K))
 
 # bugs_top.tsv only includes the first 5 columns since that's what we use in the spreadsheet
