b0971f7c181f17ef718c7ea856d8e67a36cc6975
Shrey Tiwari <smtiwari@andrew.cmu.edu>
2024-07-09 20:05:49 -0400

    Benchmarks (#38)
    * updating
    
    * updating the benchmark files
    
    * updating
    
    * updating arithmetic tests
    
    * updating readme
    
    * Plurals word list (#33)
    
    * added nltk to deps
    
    * taking the top half of tf_idf
    
    * added all files
    
    * Calculate individual TF scores; allow for selecting IDF source (#34)
    
    * added the tf_idf scores for individual keywords
    
    * add option to use repos as idf source
    
    * consitently use comments instead of comment
    
    ---------
    
    Co-authored-by: Peter Vandervelde <petervan@andrew.cmu.edu>
    
    * adding steps to run codeql analysis on benchmarks (#35)
    
    * adding steps to run codeql analysis on benchmarks
    
    * adding the pack lock file
    
    * updating readme file
    
    * resolving merge conflicts
    
    ---------
    
    Co-authored-by: Alexander Joukov <92131019+Ajoukov@users.noreply.github.com>
    Co-authored-by: Peter <33732392+r0ckwav3@users.noreply.github.com>
    Co-authored-by: Peter Vandervelde <petervan@andrew.cmu.edu>

---
+++ benchmarks/test_replace.py
@@ -0,0 +1,38 @@
+"""
+<<<<<<< HEAD
+Description: This file contains code snippets that call the replace() method on datetime objects. It tests the behavior of the method when used incorrectly.
+=======
+Description: This file contains code snippets that call the replace() method on datetime objects. It tests the behavior of the method is incorrectly used.
+>>>>>>> 0e90048 (resolving merge conflicts)
+
+Links:
+- https://stackoverflow.com/questions/39759041/replace-tzinfo-and-print-with-localtime-amends-six-minutes
+- https://stackoverflow.com/questions/1379740/pytz-localize-vs-datetime-replace
+"""
+
+import datetime
+import unittest
+
+import dateutil
+import pytz
+from hypothesis import given
+from hypothesis.strategies import datetimes
+from tzlocal import get_localzone
+
+
+class TestReplace(unittest.TestCase):
+
+    # Test that calls the replace() method on a datetime object.
+    @given(datetimes())
+    def test_replace_0(self, dt: datetime) -> None:
+        now = datetime.datetime.now()
+        local_tz = get_localzone()
+        tz1 = pytz.timezone(str(local_tz))
+        tz2 = dateutil.tz.gettz(str(local_tz))
+        shifted1 = now.replace(tzinfo=tz1)
+        shifted2 = now.replace(tzinfo=tz2)
+        assert shifted1.timestamp() == shifted2.timestamp()
+
+
+if __name__ == "__main__":
+    unittest.main()
