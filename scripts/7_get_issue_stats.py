24a80a04935afe540b37c6ab0bb77ad074d66c4c
Peter Vandervelde <petervan@andrew.cmu.edu>
2024-07-11 15:33:09 -0400

    increased char count threshold to 1000

--- scripts/7_get_issue_stats.py
+++ scripts/7_get_issue_stats.py
@@ -2,7 +2,7 @@ from __global_paths import *
 import pandas as pd
 
 K = 200
-SIZE_MIN = 500
+SIZE_MIN = 1000
 
 # bugs_with_stats_df = pd.merge(pd.read_csv(BUGS_WITH_TF_IDF_PATH), pd.read_csv(REPOS_PATH), left_on='repoName', right_on='nameWithOwner', how='left')
 # bugs_with_stats_df.to_csv(BUGS_WITH_STATS_PATH, index=False)
