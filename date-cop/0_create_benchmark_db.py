ad442ff27509452c5d4dc4a09fc72c6dc4fcfc01
r0ckwav3 <peterrockwave@gmail.com>
2024-07-29 10:23:51 -0400

    add command line args to specify operation

--- date-cop/0_create_benchmark_db.py
+++ date-cop/0_create_benchmark_db.py
@@ -1,30 +1,36 @@
 import os
+import sys
 import subprocess
 from pathlib import Path
+import pandas as pd
 
 ## helper functions ##
 def run_command(command):
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return result.stdout, result.stderr
 
-def run_command_verbose(command):
-    print(" > " + command)
-    out,err = run_command(command)
+def create_db(codeql_path, db_path, source_path):
+    run_command(f"{codeql_path} database create {db_path} --language=python --source-root={source_path} --overwrite")
 
-    if len(out) > 0:
-        print("STDOUT:")
-        print(out)
-
-    if len(err) > 0:
-        print("STDERR:")
-        print(err)
 
 ## main ##
 if __name__ == "__main__":
-    # get codeql path and find relevant folders
+    # get codeql path
     if "CODEQL_PATH" not in os.environ:
         print("Failed to find environment variable \"CODEQL_PATH\"")
         exit(1)
+    
+    # get command line args
+    if len(sys.argv) == 1:
+        generate_benchmarks = True
+        generate_repos = False
+    else:
+        generate_benchmarks = "--benchmarks" in sys.argv
+        generate_repos = "--repos" in sys.argv
+        if not (generate_benchmarks or generate_repos):
+            print(f"Usage: {sys.argv[0]} [--benchmarks] [--repos]")
+            print("If no flag is provided, default to --benchmarks")
+            exit(1)
 
     CodeQL_dir = Path(os.environ["CODEQL_PATH"])
     CodeQL_path = CodeQL_dir / "codeql"
