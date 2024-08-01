7d540a473833db9b449095740b5f09a166888df7
Ajoukov <aapteryx@gmail.com>
2024-07-31 21:10:04 -0400

    Fully automated benchmark database querying

--- date-cop/_2_run_benchmarks.py
+++ date-cop/_2_run_benchmarks.py
@@ -4,30 +4,46 @@ import subprocess
 
 from _1_generate_databases import *
 
+QL_DIR = "static-analysis/"
+RS_DIR = "results/"
+
 QUERIES_LIST = [
-    "static-analysis/deprecated_method.ql"
+    "delta_divide",
+    "delta_times_float",
+    "deprecated_method",
+    "multiple_nows",
+    "timezone_offset"
 ]
 
-def run_query(CodeQL_path, DB_path, Query_path=Path("static-analysis/test_query.ql"), Output_loc="results/test_query.csv"):
+def run_query(CodeQL_path, DB_path, Query_path=Path("static-analysis/test_query.ql"), Output_path="results/test_query.csv"):
     assert_path(DB_path)
     assert_path(Query_path)
-    return run_command(f"{CodeQL_path} database analyze {DB_path} {Query_path} --output={Output_loc} --format=csv --verbose --no-rerun=false")
+    return run_command(f"{CodeQL_path} database analyze {DB_path} {Query_path} --output={Output_path} --format=csv --verbose --no-rerun=false")
+
+def run_all_queries(CodeQL_path, DB_path):
+    print(f"Running all queries for database {DB_path}")
+    for q in QUERIES_LIST:
+        q_path = Path(QL_DIR + q + ".ql")
+        out_path = Path(RS_DIR + q + ".csv")
+        ret = run_query(CodeQL_path, DB_path, Query_path=q_path, Output_path=out_path)
+        if ret[2] != 0:
+            print(f"Error found with database {DB_path} and query {q_path}:\n{ret}")
+            exit(1)
+
 
 if __name__ == "__main__":
     CodeQL_path = get_codeql_path()
     run_benchmarks, run_repos = get_flags()
 
     if run_benchmarks:
-        print("Running benchmarks")
         DB_path = Path("./static-analysis/databases/benchmark-db")
-        print(DB_path)
 
         ## test query ##
         print("Running test query...")
         run_query(CodeQL_path, DB_path)
 
         ## test that something was found ##
-        stdout, _, _ = run_command("grep -c '' results.csv")
+        stdout, _, _ = run_command("grep -c '' results/test_query.csv")
         print(f"{stdout[:-1]} calls found")
         if (stdout == "0"):
             print("Error: No results!")
@@ -35,7 +51,7 @@ if __name__ == "__main__":
 
         print("Passed!")
 
-
+        run_all_queries(CodeQL_path, DB_path)
 
         
     if run_repos:
